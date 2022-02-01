2be56d96fb324ae336f0a51663415d60bba157e6
Duc T. Lam <ducl_13@yahoo.com>
2022-02-01 15:22:17 -0800

    Removed per Yassen request

--- app/overrides/admin_decorator.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(virtual_path:  "spree/admin/shared/_product_tabs",
-                     name:          "add_sale_prices_tab",
-                     insert_bottom: "[data-hook='admin_product_tabs']",
-                     partial:       "spree/admin/sale_prices/product_tab",
-                     disabled:      false)
